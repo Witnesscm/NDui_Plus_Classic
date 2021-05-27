28f90d4d115969826e2dd4495ff2e7e4d7c9259d
unknown <fanmicao@126.com>
2021-05-28 03:16:00 +0800

    Add skin for CChatNotifier

---
+++ Modules/Skins/CChatNotifier.lua
@@ -0,0 +1,32 @@
+local _, ns = ...
+local B, C, L, DB, P = unpack(ns)
+local S = P:GetModule("Skins")
+
+local _G = getfenv(0)
+
+function S:CChatNotifier()
+	if not IsAddOnLoaded("CChatNotifier") then return end
+
+	local frame = _G.CCNUI_MainUI
+	if not frame then return end
+
+	B.ReskinPortraitFrame(frame)
+	B.ReskinScroll(frame.scrollFrame.ScrollBar)
+
+	local items = frame.scrollFrame.items
+	for i = 1, #items do
+		B.CreateBD(items[i], .25)
+	end
+
+	for _, key in pairs({"addFrame", "editFrame"}) do
+		local subFrame = frame[key]
+		if subFrame then
+			subFrame.searchEdit:SetBackdrop(nil)
+			B.ReskinInput(subFrame.searchEdit)
+			B.Reskin(subFrame.okbutton)
+			B.Reskin(subFrame.backbutton)
+		end
+	end
+end
+
+S:RegisterSkin("CChatNotifier", S.CChatNotifier)

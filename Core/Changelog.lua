560517ec72a5884a1bd20e8e3a3f0622c6aa8138
unknown <fanmicao@126.com>
2021-05-28 03:21:39 +0800

    2.0.5

--- Core/Changelog.lua
+++ Core/Changelog.lua
@@ -2,6 +2,12 @@ local _, ns = ...
 local B, C, L, DB, P = unpack(ns)
 
 P.Changelog = [[
+v2.0.5
+[美化] 添加CChatNotifier美化
+[美化] 修复tdInspect美化
+[易用性] 修复天赋扩展无法在战斗中启用的问题
+[其他] 移除角色面板模型尺寸调整
+
 v2.0.4
 [美化] 部分美化调整
 [易用性] 添加天赋面板扩展

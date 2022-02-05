2166df73ba4ad6741f78c116c69c4dfb7222757b
Witnesscm <fanmicao@126.com>
2022-02-05 20:44:59 +0800

    Skins: update for GearMenu

--- Modules/Skins/GearMenu.lua
+++ Modules/Skins/GearMenu.lua
@@ -1,53 +1,81 @@
 local _, ns = ...
 local B, C, L, DB, P = unpack(ns)
 local S = P:GetModule("Skins")
-local Bar = B:GetModule("Actionbar")
 
 local _G = getfenv(0)
+
+local fontScale = .37
+
+local function UpdateTexture(slot)
+	local texture = slot.itemTexture
+	if texture and slot.bg then
+		texture:SetTexCoord(unpack(DB.TexCoord))
+		texture:SetInside(slot.bg)
+	end
+end
+
+local function reskinGear(slot)
+	if slot.SetBackdrop then
+		slot:SetBackdrop(nil)
+		slot.SetBackdrop = B.Dummy
+	end
+
+	slot.bg = B.SetBD(slot)
+	slot.bg:SetInside()
+
+	if slot.cooldownOverlay then slot.cooldownOverlay:SetInside(slot.bg) end
+	if slot.highlightFrame then slot.highlightFrame:SetAlpha(0) end
+
+	local hl= slot:CreateTexture(nil, "HIGHLIGHT")
+	hl:SetColorTexture(1, 1, 1, .25)
+	hl:SetInside(slot.bg)
+
+	slot:SetHighlightTexture("")
+	slot:SetNormalTexture("")
+	slot:SetPushedTexture("")
+
+	UpdateTexture(slot)
+end
 
 function S:GearMenu()
 	if not S.db["GearMenu"] then return end
 
 	local rggm = _G.rggm
+
 	if not rggm then return end
+	if not rggm.themeCoordinator then return end -- version check
 
-	local fontScale = .37
+	local origCreateGearSlot = rggm.themeCoordinator.CreateGearSlot
+	rggm.themeCoordinator.CreateGearSlot = function (...)
+		local slot = origCreateGearSlot(...)
+		if slot then
+			reskinGear(slot)
 
-	local function UpdateTexture(slot)
-		local texture = slot:GetNormalTexture()
-		if texture and slot.bg then
-			texture:SetTexCoord(unpack(DB.TexCoord))
-			texture:SetInside(slot.bg)
+			return slot
 		end
 	end
-	hooksecurefunc(rggm.uiHelper, "UpdateSlotTextureAttributes", UpdateTexture)
 
-	local function SetKeyBindingFont(slot, size)
-		slot.keyBindingText:SetFont(DB.Font[1], size * fontScale, DB.Font[3])
-	end
-	hooksecurefunc(rggm.gearBar, "UpdateGearSlotKeyBindingTextSize", SetKeyBindingFont)
+	local origCreateChangeSlot = rggm.themeCoordinator.CreateChangeSlot
+	rggm.themeCoordinator.CreateChangeSlot = function (...)
+		local slot = origCreateChangeSlot(...)
+		if slot then
+			reskinGear(slot)
 
-	local function reskinGear(slot)
-		slot:SetBackdrop(nil)
-		slot.SetBackdrop = B.Dummy
-		slot.bg = B.SetBD(slot)
-		slot.bg:SetInside()
-		slot.cooldownOverlay:SetInside(slot.bg)
-		slot.highlightFrame:SetAlpha(0)
-
-		local hl= slot:CreateTexture(nil, "HIGHLIGHT")
-		hl:SetColorTexture(1, 1, 1, .25)
-		hl:SetInside(slot.bg)
-		UpdateTexture(slot)
+			return slot
+		end
 	end
 
-	local origCreateGearSlot = rggm.gearBar.CreateGearSlot
-	rggm.gearBar.CreateGearSlot = function (...)
-		local slot = origCreateGearSlot(...)
-		reskinGear(slot)
+	local origCreateTrinketSlot = rggm.themeCoordinator.CreateTrinketSlot
+	rggm.themeCoordinator.CreateTrinketSlot = function (...)
+		local slot = origCreateTrinketSlot(...)
+		if slot then
+			reskinGear(slot)
 
-		return slot
+			return slot
+		end
 	end
+
+	hooksecurefunc(rggm.themeCoordinator, "UpdateSlotTextureAttributes", UpdateTexture)
 
 	local origCreateKeyBindingText = rggm.gearBar.CreateKeyBindingText
 	rggm.gearBar.CreateKeyBindingText = function(slot, size)
@@ -57,13 +85,10 @@ function S:GearMenu()
 		return keybinding
 	end
 
-	local origCreateChangeSlot = rggm.gearBarChangeMenu.CreateChangeSlot
-	rggm.gearBarChangeMenu.CreateChangeSlot = function(...)
-		local slot = origCreateChangeSlot(...)
-		reskinGear(slot)
-
-		return slot
+	local function SetKeyBindingFont(slot, size)
+		slot.keyBindingText:SetFont(DB.Font[1], size * fontScale, DB.Font[3])
 	end
+	hooksecurefunc(rggm.gearBar, "UpdateGearSlotKeyBindingTextSize", SetKeyBindingFont)
 end
 
 S:RegisterSkin("GearMenu", S.GearMenu)

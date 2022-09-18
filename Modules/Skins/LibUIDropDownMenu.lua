a7990104fd100c40cdf6021364b2c13f62317e4c
Witnesscm <fanmicao@126.com>
2022-09-18 18:05:04 +0800

    Skins: LibUIDropDownMenuQuestie

--- Modules/Skins/LibUIDropDownMenu.lua
+++ Modules/Skins/LibUIDropDownMenu.lua
@@ -5,15 +5,18 @@ local r, g, b = DB.r, DB.g, DB.b
 
 local _G = getfenv(0)
 
-local function reskinDropDownMenu(level)
-	local listFrame = _G["L_DropDownList"..level]
+local function reskinDropDownMenu(level, isQuestie)
+	if not level then level = 1 end
+
+	local listFrameName = isQuestie and "L_DropDownListQuestie"..level or "L_DropDownList"..level
+	local listFrame = _G[listFrameName]
 
 	if not listFrame.__bg then
 		listFrame.__bg = B.SetBD(listFrame, .7)
 	end
 
 	for _, key in pairs({"Backdrop", "MenuBackdrop", "Border"}) do
-		local backdrop = listFrame[key] or _G["L_DropDownList"..level..key]
+		local backdrop = listFrame[key] or _G[listFrameName..key]
 		if backdrop and not backdrop.__styled then
 			backdrop:Hide()
 			backdrop.Show = B.Dummy
@@ -22,14 +25,15 @@ local function reskinDropDownMenu(level)
 		end
 	end
 
-	for i = 1, L_UIDROPDOWNMENU_MAXBUTTONS do
-		local bu = _G["L_DropDownList"..level.."Button"..i]
+	local maxButtons = isQuestie and _G.L_UIDROPDOWNMENUQUESTIE_MAXBUTTONS or _G.L_UIDROPDOWNMENU_MAXBUTTONS
+	for i = 1, maxButtons do
+		local bu = _G[listFrameName.."Button"..i]
 		local _, _, _, x = bu:GetPoint()
 		if bu:IsShown() and x then
-			local check = _G["L_DropDownList"..level.."Button"..i.."Check"]
-			local uncheck = _G["L_DropDownList"..level.."Button"..i.."UnCheck"]
-			local hl = _G["L_DropDownList"..level.."Button"..i.."Highlight"]
-			local arrow = _G["L_DropDownList"..level.."Button"..i.."ExpandArrow"]
+			local check = _G[listFrameName.."Button"..i.."Check"]
+			local uncheck = _G[listFrameName.."Button"..i.."UnCheck"]
+			local hl = _G[listFrameName.."Button"..i.."Highlight"]
+			local arrow = _G[listFrameName.."Button"..i.."ExpandArrow"]
 
 			if not bu.bg then
 				bu.bg = B.CreateBDFrame(bu)
@@ -70,19 +74,23 @@ local function reskinDropDownMenu(level)
 end
 
 function S:LibUIDropDownMenu()
-	local LibDD = LibStub("LibUIDropDownMenu-4.0", true)
-	if LibDD then
-		hooksecurefunc(LibDD, "ToggleDropDownMenu", function(self, level, ...)
-			if not level then level = 1 end
-
+	local LibDropMenu = LibStub("LibUIDropDownMenu-4.0", true)
+	if LibDropMenu then
+		hooksecurefunc(LibDropMenu, "ToggleDropDownMenu", function(self, level, ...)
 			reskinDropDownMenu(level)
 		end)
 	end
 
+	local LibDropMenuQuestie = LibStub("LibUIDropDownMenuQuestie-4.0", true)
+	if LibDropMenuQuestie then
+		hooksecurefunc(LibDropMenuQuestie, "ToggleDropDownMenu", function(self, level, ...)
+			reskinDropDownMenu(level, true)
+		end)
+	end
+
+	-- LibUIDropDownMenu-2.0
 	if _G.L_ToggleDropDownMenu then
 		hooksecurefunc(_G, "L_ToggleDropDownMenu", function(level, ...)
-			if not level then level = 1 end
-
 			reskinDropDownMenu(level)
 		end)
 	end
